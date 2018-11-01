620ade9c061080814efc3ff796dcb3f51e9220f2
LEO CANTU <rl300126@rlas-macair-39.local>
2018-11-01 10:24:06 -0500

    Initial Commit

---
+++ scopeHackwich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  scopeHackwich
+//
+//  Created by LEO CANTU on 11/1/18.
+//  Copyright © 2018 LEO CANTU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
